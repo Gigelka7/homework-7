a85180220ccafeebc89237015b400d03039086d2
Kolesnik Anton <kolesnikas2006@gmail.com>
2022-11-24 20:43:39 +0500

    first commit

---
+++ 47/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 5;
+int b = 3;
+double[,] n = new double[a, b];
+FillArrayRandomNumbers(n);
+PrintArray(n);
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
